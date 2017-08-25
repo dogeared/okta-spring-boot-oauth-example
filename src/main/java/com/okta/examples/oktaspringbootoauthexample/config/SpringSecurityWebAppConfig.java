2480743ec0fea0d62893b0ebb5e64466808e0021
Micah Silverman <micah@afitnerd.com>
2017-08-24 23:31:09 -0400

    Added support for authorities (groups)

--- src/main/java/com/okta/examples/oktaspringbootoauthexample/config/SpringSecurityWebAppConfig.java
+++ src/main/java/com/okta/examples/oktaspringbootoauthexample/config/SpringSecurityWebAppConfig.java
@@ -4,11 +4,21 @@ package com.okta.examples.oktaspringbootoauthexample.config;
 import com.okta.examples.oktaspringbootoauthexample.jwt.JWTFilter;
 import com.okta.examples.oktaspringbootoauthexample.jwt.TokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableOAuth2Sso
@@ -17,6 +27,9 @@ public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     TokenProvider tokenProvider;
 
+    @Value("#{ @environment['okta.moreSecure.authorities'] }")
+    String[] moreSecureAuthorities;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
@@ -24,10 +37,24 @@ public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
             .antMatcher("/**")
             .authorizeRequests()
             .antMatchers("/").permitAll()
+            .antMatchers("/more-secure").hasAnyAuthority(moreSecureAuthorities)
             .anyRequest().authenticated()
             .and()
             .logout().logoutSuccessUrl("/")
             .and()
             .addFilterBefore(new JWTFilter(tokenProvider), BasicAuthenticationFilter.class);
+    }
+
+    @Bean
+    public AuthoritiesExtractor authoritiesExtractor(OAuth2RestOperations template) {
+        return map -> {
+            List<String> groups = (List<String>) map.get("groups");
+            return AuthorityUtils.commaSeparatedStringToAuthorityList(String.join(",", groups));
+        };
+    }
+
+    @Bean
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ProtectedResourceDetails resource, OAuth2ClientContext context) {
+        return new OAuth2RestTemplate(resource, context);
     }
 }
--- src/main/java/com/okta/examples/oktaspringbootoauthexample/controller/HelloController.java
+++ src/main/java/com/okta/examples/oktaspringbootoauthexample/controller/HelloController.java
@@ -13,6 +13,9 @@ public class HelloController {
     @Value("#{ @environment['okta.tenant.uri'] }")
     private String oktaTenantUri;
 
+    @Value("#{ @environment['okta.moreSecure.authorities'] }")
+    String[] moreSecureAuthorities;
+
     @RequestMapping("/")
     public String home() {
         return "home";
@@ -22,6 +25,13 @@ public class HelloController {
     public String secure(Model model, Principal user) {
         model.addAttribute("user", user);
         model.addAttribute("oktaTenantUri", oktaTenantUri);
+        model.addAttribute("authorities", moreSecureAuthorities);
         return "secure";
+    }
+
+    // you have to be a member of the groups defined in SpringSecurityWebAppConfig
+    @RequestMapping("/more-secure")
+    public String moreSecure(Model model, Principal user) {
+        return secure(model, user);
     }
 }
